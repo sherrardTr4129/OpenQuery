3fe61d881684cbf2f9696709ebabdaee26b26b24
Trevor Sherrard <Tws4129rit.edu>
2015-09-28 12:24:11 -0400

    removed uneeded files from testing

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='CSHLDAP',
-      version='0.2',
-      description='LDAP Wrapper for CSH',
-      author='Matt Gambogi',
-      author_email='m@csh.rit.edu',
-      url='http://www.github.com/gambogi/CSHLDAP',
-      py_modules=['CSHLDAP'],
-      license="MIT")
