b3c0d8bbc057e0ebda38a4f40557d9b76afff1bc
Trevor <sherrardTr@gmail.com>
2015-10-12 23:36:02 -0400

    Update Speak.sh

--- utils/Speak.sh
+++ utils/Speak.sh
@@ -1,4 +1,4 @@
 read -p "Enter your phrase : " name
 pico2wave -w msg.wav "$name"
-sshpass -p "faceharold" scp msg.wav pi@faceharold.csh.rit.edu:~/
-sshpass -p "faceharold" ssh pi@faceharold.csh.rit.edu aplay msg.wav
+sshpass -p "<password>" scp msg.wav pi@faceharold.csh.rit.edu:~/
+sshpass -p "<password>" ssh pi@faceharold.csh.rit.edu aplay msg.wav
